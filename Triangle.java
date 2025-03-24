7f7e37c71abaa0f91978497f5f8ae8103fb283ac
sanjanasujay <sanjana121123@gmail.com>
2025-03-24 22:45:22 +0530

    Create triangle program

---
+++ Triangle.java
@@ -0,0 +1,15 @@
+public class Triangle {
+    public static void main(String[] args) {
+        int i, j;
+        int r=4;
+        
+        for(i=1;i<=r;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
