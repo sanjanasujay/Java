402007c61e299266cf0f92d99731e63ae8904d17
sanjanasujay <sanjana121123@gmail.com>
2025-03-24 22:26:49 +0530

    Create solid rectangle nested loops

--- SolidRectangle.java
+++ SolidRectangle.java
@@ -1,5 +1,15 @@
 public class SolidRectangle {
     public static void main(String[] args) {
-        
+        int i, j;
+        //row
+        for (i=1;i<5;i++)
+        {
+            //col
+            for(j=1;j<6;j++)
+            {
+            System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
