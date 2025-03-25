ebe5418f528c148c8888fdae5c25dbb9a8f14686
sanjanasujay <sanjana121123@gmail.com>
2025-03-25 22:13:48 +0530

    Write code for zeroONe

--- ZeroOne.java
+++ ZeroOne.java
@@ -1,5 +1,20 @@
 public class ZeroOne {
     public static void main(String[] args) {
         int i,j,c;
+        for(i=1;i<=5;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                if((i+j)%2==0)
+                {
+                    System.out.print("1");
+                }
+                else
+                {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
     }
 }
