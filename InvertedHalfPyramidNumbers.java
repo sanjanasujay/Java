b869b6a00d102b43e2e4365897a88c41bc170595
sanjanasujay <sanjana121123@gmail.com>
2025-03-25 16:34:39 +0530

    Draft code for 2 patterns

---
+++ InvertedHalfPyramidNumbers.java
@@ -0,0 +1,16 @@
+public class InvertedHalfPyramidNumbers {
+  
+        public static void main(String[] args) {
+            int r=4;
+            int i,j;
+            for(i=1;i<=r;i++)
+            {
+                for(j=r;j>=1;j--)
+                {
+                    System.out.print("");
+                }
+                System.out.println();
+            }
+        }
+    
+}
---
+++ ZeroOne.java
@@ -0,0 +1,5 @@
+public class ZeroOne {
+    public static void main(String[] args) {
+        int i,j,c;
+    }
+}
