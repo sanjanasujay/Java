8c9e9ca8796ee2bfe9454c2f537a5aed8abd7a3d
sanjanasujay <sanjana121123@gmail.com>
2025-03-25 16:11:57 +0530

    Create half pyramids with numbers

---
+++ HalfPyramidNumbers.java
@@ -0,0 +1,17 @@
+public class HalfPyramidNumbers {
+    
+        public static void main(String[] args) {
+            int r=5;
+            int i,j;
+            for(i=1;i<=r;i++)
+            {
+                for(j=1;j<=i;j++)
+                {
+                    System.out.print(+j);
+                }
+                System.out.println();
+            }
+        }
+    
+    
+}
